57de5489e0bdfe878d3251c4f72255ea7ba0449f
sublun <sublun@126.com>
2020-08-09 15:16:50 +0800

    day8_8提交

---
+++ day8_8/src/main/java/cn/itcast/SpringBootRunner.java
@@ -0,0 +1,16 @@
+package cn.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther ch
+ * @date 2020/8/8 23:07
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class SpringBootRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRunner.class,args);
+    }
+}
